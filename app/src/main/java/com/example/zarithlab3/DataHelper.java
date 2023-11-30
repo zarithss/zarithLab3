9f0e6101a6c0c7d88bcb813632420825413a5a6e
zarithLab1 <2022496474@student.uitm.edu.my>
2023-11-30 22:51:52 +0800

    firstcommit

---
+++ app/src/main/java/com/example/zarithlab3/DataHelper.java
@@ -0,0 +1,24 @@
+package com.example.zarithlab3;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "personalbiodata.db";
+    private static final int DATABASE_VERSION = 1;
+    public DataHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "create table biodata(no integer primary key, name text null,dob text null," +
+                "address text null);";
+        Log.d("Data","OnCreate:"+sql);
+        db.execSQL(sql);
+    }
+    public void onUpgrade(SQLiteDatabase arg0,int arg1,int arg2){
+    }
+}
